3bdf007014e478bf1e80b676c59a0d12f69780a0
Mkous315 <90722650+Mkous315@users.noreply.github.com>
2024-11-26 19:34:19 -0700

    Update Deliverable.sql

--- Deliverable.sql
+++ Deliverable.sql
@@ -1,1 +1,37 @@
+CREATE TABLE Buildings(
+  BuildingID INT PRIMARY KEY,
+  Name VARCHAR (40),
+  Address VARCHAR(40),
+  ManagerName VARCHAR(40)
+);
 
+CREATE TABLE Apartment(
+ApartmentId INT PRIMARY KEY,
+BuildingID INT,
+Type VARCHAR (40),
+ApartmentNumber INT,
+Rent DECIMAL(10, 2),
+Availability Boolean,
+  
+FOREIGN KEY (BuildingID) REFRENCES Buildings(BuildingID)
+);
+
+CREATE TABLE Booking(
+BookingID INT PRIMARY KEY,
+GuestID INT,
+ApartmentID INT,
+CheckInDate DATE,
+CheckOutDate DATE,
+Status VARCHAR(40),
+
+FOREIGN KEY (GuestID) REFERENCES Guest(GuestID),
+FOREIGN KEY (ApartmentID) REFERENCES Apartment(ApartmentID)
+);
+
+CREATE TABLE Guest(
+GuestID INT PRIMARY KEY,
+FirstName VARCHAR (40),
+LastName VARCHAR (40),
+Phone VARCHAR(15),
+Email VARCHAR(100) 
+);
